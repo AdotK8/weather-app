7d6dd8cd4a724e42dfd08cc900d398b29868e682
Ahmed <ahmedkhan895.ak@gmail.com>
2023-12-01 00:38:35 +0000

    simple UI with dummy data added, need to fix forecast UI

--- src/timeConversion.js
+++ src/timeConversion.js
@@ -2,8 +2,10 @@ export default function epochToTime(x, shift) {
   const shiftedTime = x + shift;
   const date = new Date(shiftedTime * 1000);
   const time = date.getHours();
+  const day = date.getDay();
+  const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   if (time < 10) {
-    return `0${time}:00`;
+    return `${dayNames[day]}|0${time}:00`;
   }
-  return `${time}:00`;
+  return `${dayNames[day]}|${time}:00`;
 }
