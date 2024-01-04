113aeb8f81d5e2e9f9b784540a5908e22924265d
Ahmed <ahmedkhan895.ak@gmail.com>
2024-01-04 21:05:41 +0000

    hopefully fixed image issue

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import retrieveData from "./retrieveData";
 import "./styles/styles.css";
 import searchAndSubmit from "./searchButton";
 import toggleButton from "./tempConversionButton";
+import gitPic from "./images/github.svg";
+import magPic from "./images/magnify.svg";
+
+const git = document.getElementById("git");
+const mag = document.getElementById("magnify-icon");
+
+git.src = gitPic;
+mag.src = magPic;
 
 retrieveData("london");
 searchAndSubmit();
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-        ],
-      },
-}
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
 
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    assetModuleFilename: "[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(svg|jpg|png|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
